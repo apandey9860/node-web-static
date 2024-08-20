9910e1c78acd2ff62ea4cf3bc23091802b3d98d6
AshishManandhar9 <aashishmanandhar99@gmail.com>
2024-08-20 16:26:43 +0545

    person-table-added

---
+++ database/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO PERSON.USER_ACCESS (USER_ACCESS_LEVEL, USER_ACCESS_DESCRIPTION) VALUES
+    (1, 'Viewer', 'Can View Website'),
+    (2, 'Customer', 'Can use buy or repair services'),
+    (3, 'Admin','All privilege and can create other users');
