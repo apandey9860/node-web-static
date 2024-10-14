162559e89b773a6fc86be67d588dfba8c83fbe31
Aditya8014 <adityapk60@gmail.com>
2024-10-14 16:18:03 +0545

    added delete and edit icon

--- src/components/displayTradeProducts.js
+++ src/components/displayTradeProducts.js
@@ -1,37 +1,42 @@
 function fetchProductData() {
-    fetch('/trade/getAllProductDetails')
-        .then(response => response.json())
-        .then(data => {
-            const tbody = document.querySelector('#productTableBody');
-            tbody.innerHTML = ''; // Clear existing rows
-            data.forEach(product => {
-                const row = document.createElement('tr');
-                row.innerHTML = `
+  fetch("/trade/getAllProductDetails")
+    .then((response) => response.json())
+    .then((data) => {
+      const tbody = document.querySelector("#productTableBody");
+      tbody.innerHTML = ""; // Clear existing rows
+      data.forEach((product) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
                     <td><span class="custom-checkbox"><input type="checkbox" class="select-product" value="${product.product_id}"><label></label></span></td>
                     <td>${product.product_name}</td>
                     <td>${product.short_description}</td>
                     <td><img src="../${product.picture_data}" alt="${product.product_name}" style="width: 50px;"></td>
                     <td>${product.category_name}</td>
+
                     <td>
-                        <button id="editProductBtn" class="edit-product-btn" data-toggle="modal" data-id="${product.product_id}"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></button>
-                        <button id="deleteProductBtn" class="delete-product-btn" data-toggle="modal" data-id="${product.product_id}"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></button>
+                       <button id="editProductBtn" class="edit-product-btn" data-toggle="modal" data-id="${product.product_id}">
+                       <img src="./images/home-page/edit.png" style="width:20px;height:20px;">
+                       </button>
+                       <button id="deleteProductBtn" class="delete-product-btn" data-toggle="modal" data-id="${product.product_id}">
+                       <img src="./images/home-page/delete.png" style="width:20px;height:20px;">
+                       </button>
                     </td>
                 `;
-                tbody.appendChild(row);
-            });
+        tbody.appendChild(row);
+      });
 
-             // Attach event listeners to dynamically added buttons
-             document.querySelectorAll('.edit-product-btn').forEach(button => {
-                button.addEventListener('click', editProductBtnClick);
-            });
+      // Attach event listeners to dynamically added buttons
+      document.querySelectorAll(".edit-product-btn").forEach((button) => {
+        button.addEventListener("click", editProductBtnClick);
+      });
 
-            document.querySelectorAll('.delete-product-btn').forEach(button => {
-                button.addEventListener('click', deleteProductBtnClick);
-            });
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+      document.querySelectorAll(".delete-product-btn").forEach((button) => {
+        button.addEventListener("click", deleteProductBtnClick);
+      });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
 // Call fetchProductData initially to populate the table
